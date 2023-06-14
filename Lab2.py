f788dee4d5f8f620a4d0c4ebe468083454c258dc
133931212-myseneca <133059127+133931212-myseneca@users.noreply.github.com>
2023-06-13 20:13:53 -0400

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,12 @@
+import sys
+
+def print_arguments():
+    script_name = sys.argv[0]
+    print("Script name:", script_name)
+
+    if len(sys.argv) > 1:
+        arguments = ' '.join(sys.argv[1:])
+        print("Script name and arguments:", script_name, arguments)
+
+print_arguments()
 
